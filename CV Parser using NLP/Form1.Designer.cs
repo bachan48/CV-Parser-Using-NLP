f0cc18a94b858906be65ffb1be08b93315292ee1
Abishek Sitaula <abisheksitaula@hotmail.com>
2018-01-20 17:03:33 +0545

    Lol

--- CV Parser using NLP/Form1.Designer.cs
+++ CV Parser using NLP/Form1.Designer.cs
@@ -57,10 +57,9 @@
             this.headerLabel.AutoSize = true;
             this.headerLabel.Font = new System.Drawing.Font("Trebuchet MS", 33.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.headerLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(253)))), ((int)(((byte)(18)))));
-            this.headerLabel.Location = new System.Drawing.Point(318, 17);
-            this.headerLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.headerLabel.Location = new System.Drawing.Point(159, 9);
             this.headerLabel.Name = "headerLabel";
-            this.headerLabel.Size = new System.Drawing.Size(891, 111);
+            this.headerLabel.Size = new System.Drawing.Size(448, 57);
             this.headerLabel.TabIndex = 0;
             this.headerLabel.Text = "CV Parser Using NLP";
             // 
@@ -68,30 +67,27 @@
             // 
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label2.Location = new System.Drawing.Point(2, 135);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(1, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1516, 4);
+            this.label2.Size = new System.Drawing.Size(758, 2);
             this.label2.TabIndex = 1;
             // 
             // label4
             // 
             this.label4.BackColor = System.Drawing.Color.White;
             this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label4.Location = new System.Drawing.Point(988, 138);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(494, 72);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(4, 765);
+            this.label4.Size = new System.Drawing.Size(2, 398);
             this.label4.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.BackColor = System.Drawing.Color.White;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label3.Location = new System.Drawing.Point(2, 225);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(1, 117);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(1516, 4);
+            this.label3.Size = new System.Drawing.Size(758, 2);
             this.label3.TabIndex = 4;
             // 
             // jobRequirementLabel
@@ -99,10 +95,9 @@
             this.jobRequirementLabel.AutoSize = true;
             this.jobRequirementLabel.Font = new System.Drawing.Font("Trebuchet MS", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.jobRequirementLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(186)))), ((int)(((byte)(110)))));
-            this.jobRequirementLabel.Location = new System.Drawing.Point(248, 152);
-            this.jobRequirementLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.jobRequirementLabel.Location = new System.Drawing.Point(124, 79);
             this.jobRequirementLabel.Name = "jobRequirementLabel";
-            this.jobRequirementLabel.Size = new System.Drawing.Size(406, 63);
+            this.jobRequirementLabel.Size = new System.Drawing.Size(208, 32);
             this.jobRequirementLabel.TabIndex = 5;
             this.jobRequirementLabel.Text = "Job Requirement";
             // 
@@ -111,10 +106,9 @@
             this.topMatchingCVsLabel.AutoSize = true;
             this.topMatchingCVsLabel.Font = new System.Drawing.Font("Trebuchet MS", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.topMatchingCVsLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(186)))), ((int)(((byte)(110)))));
-            this.topMatchingCVsLabel.Location = new System.Drawing.Point(1060, 152);
-            this.topMatchingCVsLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.topMatchingCVsLabel.Location = new System.Drawing.Point(530, 79);
             this.topMatchingCVsLabel.Name = "topMatchingCVsLabel";
-            this.topMatchingCVsLabel.Size = new System.Drawing.Size(413, 63);
+            this.topMatchingCVsLabel.Size = new System.Drawing.Size(211, 32);
             this.topMatchingCVsLabel.TabIndex = 6;
             this.topMatchingCVsLabel.Text = "Top Matching CVs";
             // 
@@ -123,22 +117,20 @@
             this.jobTitleLabel.AutoSize = true;
             this.jobTitleLabel.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.jobTitleLabel.ForeColor = System.Drawing.Color.White;
-            this.jobTitleLabel.Location = new System.Drawing.Point(70, 262);
-            this.jobTitleLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.jobTitleLabel.Location = new System.Drawing.Point(35, 136);
             this.jobTitleLabel.Name = "jobTitleLabel";
-            this.jobTitleLabel.Size = new System.Drawing.Size(123, 33);
+            this.jobTitleLabel.Size = new System.Drawing.Size(67, 18);
             this.jobTitleLabel.TabIndex = 7;
-            this.jobTitleLabel.Text = "Job Title";
+            this.jobTitleLabel.Text = "Job Title:";
             // 
             // educationalQualificationLabel
             // 
             this.educationalQualificationLabel.AutoSize = true;
             this.educationalQualificationLabel.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.educationalQualificationLabel.ForeColor = System.Drawing.Color.White;
-            this.educationalQualificationLabel.Location = new System.Drawing.Point(70, 387);
-            this.educationalQualificationLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.educationalQualificationLabel.Location = new System.Drawing.Point(35, 201);
             this.educationalQualificationLabel.Name = "educationalQualificationLabel";
-            this.educationalQualificationLabel.Size = new System.Drawing.Size(578, 33);
+            this.educationalQualificationLabel.Size = new System.Drawing.Size(294, 18);
             this.educationalQualificationLabel.TabIndex = 8;
             this.educationalQualificationLabel.Text = "Educational Qualification (subject Major Only)";
             // 
@@ -147,10 +139,9 @@
             this.skillsLabel.AutoSize = true;
             this.skillsLabel.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.skillsLabel.ForeColor = System.Drawing.Color.White;
-            this.skillsLabel.Location = new System.Drawing.Point(70, 508);
-            this.skillsLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.skillsLabel.Location = new System.Drawing.Point(35, 264);
             this.skillsLabel.Name = "skillsLabel";
-            this.skillsLabel.Size = new System.Drawing.Size(381, 33);
+            this.skillsLabel.Size = new System.Drawing.Size(193, 18);
             this.skillsLabel.TabIndex = 9;
             this.skillsLabel.Text = "Skills (Seperate with Commas)";
             // 
@@ -159,10 +150,9 @@
             this.educationLabel.AutoSize = true;
             this.educationLabel.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.educationLabel.ForeColor = System.Drawing.Color.White;
-            this.educationLabel.Location = new System.Drawing.Point(70, 635);
-            this.educationLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.educationLabel.Location = new System.Drawing.Point(35, 330);
             this.educationLabel.Name = "educationLabel";
-            this.educationLabel.Size = new System.Drawing.Size(323, 33);
+            this.educationLabel.Size = new System.Drawing.Size(164, 18);
             this.educationLabel.TabIndex = 10;
             this.educationLabel.Text = "Experience (Field, Years)";
             // 
@@ -171,10 +161,9 @@
             this.cvDirectoryLabel.AutoSize = true;
             this.cvDirectoryLabel.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cvDirectoryLabel.ForeColor = System.Drawing.Color.White;
-            this.cvDirectoryLabel.Location = new System.Drawing.Point(70, 765);
-            this.cvDirectoryLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.cvDirectoryLabel.Location = new System.Drawing.Point(35, 398);
             this.cvDirectoryLabel.Name = "cvDirectoryLabel";
-            this.cvDirectoryLabel.Size = new System.Drawing.Size(169, 33);
+            this.cvDirectoryLabel.Size = new System.Drawing.Size(85, 18);
             this.cvDirectoryLabel.TabIndex = 11;
             this.cvDirectoryLabel.Text = "CV Directory";
             // 
@@ -182,20 +171,18 @@
             // 
             this.jobTitleTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.jobTitleTextbox.Font = new System.Drawing.Font("Trebuchet MS", 10F);
-            this.jobTitleTextbox.Location = new System.Drawing.Point(76, 321);
-            this.jobTitleTextbox.Margin = new System.Windows.Forms.Padding(6);
+            this.jobTitleTextbox.Location = new System.Drawing.Point(38, 167);
             this.jobTitleTextbox.Name = "jobTitleTextbox";
-            this.jobTitleTextbox.Size = new System.Drawing.Size(488, 31);
+            this.jobTitleTextbox.Size = new System.Drawing.Size(244, 16);
             this.jobTitleTextbox.TabIndex = 12;
             // 
             // educationQualificationTextbox
             // 
             this.educationQualificationTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.educationQualificationTextbox.Font = new System.Drawing.Font("Trebuchet MS", 10F);
-            this.educationQualificationTextbox.Location = new System.Drawing.Point(76, 442);
-            this.educationQualificationTextbox.Margin = new System.Windows.Forms.Padding(6);
+            this.educationQualificationTextbox.Location = new System.Drawing.Point(38, 230);
             this.educationQualificationTextbox.Name = "educationQualificationTextbox";
-            this.educationQualificationTextbox.Size = new System.Drawing.Size(488, 31);
+            this.educationQualificationTextbox.Size = new System.Drawing.Size(244, 16);
             this.educationQualificationTextbox.TabIndex = 13;
             this.educationQualificationTextbox.Text = "Example: Bachelor in Computer";
             this.educationQualificationTextbox.TextChanged += new System.EventHandler(this.educationQualificationTextbox_TextChanged);
@@ -204,10 +191,9 @@
             // 
             this.skillsTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.skillsTextbox.Font = new System.Drawing.Font("Trebuchet MS", 10F);
-            this.skillsTextbox.Location = new System.Drawing.Point(76, 567);
-            this.skillsTextbox.Margin = new System.Windows.Forms.Padding(6);
+            this.skillsTextbox.Location = new System.Drawing.Point(38, 295);
             this.skillsTextbox.Name = "skillsTextbox";
-            this.skillsTextbox.Size = new System.Drawing.Size(488, 31);
+            this.skillsTextbox.Size = new System.Drawing.Size(244, 16);
             this.skillsTextbox.TabIndex = 14;
             this.skillsTextbox.Text = "Skill, skill";
             // 
@@ -215,10 +201,9 @@
             // 
             this.experienceTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.experienceTextBox.Font = new System.Drawing.Font("Trebuchet MS", 10F);
-            this.experienceTextBox.Location = new System.Drawing.Point(76, 700);
-            this.experienceTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.experienceTextBox.Location = new System.Drawing.Point(38, 364);
             this.experienceTextBox.Name = "experienceTextBox";
-            this.experienceTextBox.Size = new System.Drawing.Size(488, 31);
+            this.experienceTextBox.Size = new System.Drawing.Size(244, 16);
             this.experienceTextBox.TabIndex = 15;
             this.experienceTextBox.Text = "Example: Programming, 3";
             // 
@@ -226,10 +211,9 @@
             // 
             this.cvDirectoryTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.cvDirectoryTextbox.Font = new System.Drawing.Font("Trebuchet MS", 10F);
-            this.cvDirectoryTextbox.Location = new System.Drawing.Point(76, 823);
-            this.cvDirectoryTextbox.Margin = new System.Windows.Forms.Padding(6);
+            this.cvDirectoryTextbox.Location = new System.Drawing.Point(38, 428);
             this.cvDirectoryTextbox.Name = "cvDirectoryTextbox";
-            this.cvDirectoryTextbox.Size = new System.Drawing.Size(488, 31);
+            this.cvDirectoryTextbox.Size = new System.Drawing.Size(244, 16);
             this.cvDirectoryTextbox.TabIndex = 16;
             this.cvDirectoryTextbox.Text = "Input CV Directory";
             // 
@@ -238,10 +222,9 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(253)))), ((int)(((byte)(18)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(608, 810);
-            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Location = new System.Drawing.Point(304, 421);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(150, 62);
+            this.button1.Size = new System.Drawing.Size(75, 32);
             this.button1.TabIndex = 17;
             this.button1.Text = "Browse";
             this.button1.UseVisualStyleBackColor = false;
@@ -251,10 +234,9 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(253)))), ((int)(((byte)(18)))));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(804, 810);
-            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Location = new System.Drawing.Point(402, 421);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 62);
+            this.button2.Size = new System.Drawing.Size(75, 32);
             this.button2.TabIndex = 18;
             this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = false;
@@ -272,12 +254,11 @@
             this.cvTitle});
             this.matchingCVTable.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.matchingCVTable.GridColor = System.Drawing.SystemColors.ActiveBorder;
-            this.matchingCVTable.Location = new System.Drawing.Point(1016, 254);
-            this.matchingCVTable.Margin = new System.Windows.Forms.Padding(6);
+            this.matchingCVTable.Location = new System.Drawing.Point(511, 132);
             this.matchingCVTable.Name = "matchingCVTable";
             this.matchingCVTable.RowHeadersVisible = false;
             this.matchingCVTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.matchingCVTable.Size = new System.Drawing.Size(467, 618);
+            this.matchingCVTable.Size = new System.Drawing.Size(234, 321);
             this.matchingCVTable.TabIndex = 19;
             // 
             // cvRank
@@ -294,10 +275,10 @@
             // 
             // form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(1520, 904);
+            this.ClientSize = new System.Drawing.Size(759, 469);
             this.Controls.Add(this.matchingCVTable);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -318,7 +299,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.headerLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(6);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "form";
