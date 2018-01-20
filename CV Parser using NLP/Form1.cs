aabf27ce5222711d0de1edd75ee2427001ca48c6
Bachan Ghimire <bachanghimire@sportsconnect.io>
2018-01-20 16:58:30 +0545

    initial commit

---
+++ CV Parser using NLP/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CV_Parser_using_NLP
+{
+    public partial class form : Form
+    {
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        private void educationQualificationTextbox_TextChanged(object sender, EventArgs e)
+        {
+            educationQualificationTextbox.Text = "";
+        }
+    }
+}
